4ecdaf7125d06e9287a49892516473d927e2714f
Zzrk <1252124617@qq.com>
2023-09-14 01:32:03 +0800

    feat: render image from buffer

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
 fn main() -> eframe::Result<()> {
-    let default_options = eframe::NativeOptions::default();
+    // let default_options = eframe::NativeOptions::default();
     let fullscreen_options = eframe::NativeOptions {
         fullscreen: true,
         ..Default::default()
     };
 
-    eframe::run_native(
-        "snipaste",
-        default_options,
-        Box::new(|_cc| Box::new(snipaste::MainApp::default())),
-    )?;
+    // eframe::run_native(
+    //     "snipaste",
+    //     default_options,
+    //     Box::new(|_cc| Box::new(snipaste::MainApp::default())),
+    // )?;
 
     eframe::run_native(
         "screenshot",
--- src/screenshot_app.rs
+++ src/screenshot_app.rs
@@ -1,4 +1,7 @@
+use egui::ColorImage;
 use egui_extras::RetainedImage;
+use image::{ImageBuffer, Rgba};
+use screenshots::Screen;
 
 pub struct ScreenshotApp {
     image: RetainedImage,
@@ -6,12 +9,15 @@ pub struct ScreenshotApp {
 
 impl Default for ScreenshotApp {
     fn default() -> Self {
+        // 从 Buffer 创建 ColorImage
+        let image = capture_screens();
+        let color_image = ColorImage::from_rgba_unmultiplied(
+            [image.width() as usize, image.height() as usize],
+            image.as_ref(),
+        );
+        // 从 ColorImage 创建 RetainedImage
         Self {
-            image: RetainedImage::from_image_bytes(
-                "capture.png",
-                include_bytes!("../target/capture.png"),
-            )
-            .unwrap(),
+            image: RetainedImage::from_color_image("capture.png", color_image),
         }
     }
 }
@@ -20,4 +26,9 @@ impl eframe::App for ScreenshotApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| self.image.show(ui));
     }
+}
+
+pub fn capture_screens() -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    let screens = Screen::all().unwrap();
+    screens[0].capture().unwrap()
 }
